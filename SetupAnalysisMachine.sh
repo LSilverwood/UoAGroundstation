5618009f75e2a8ee8c262b3ab9037faf9cb84c13
LSilverwood <58280640+LSilverwood@users.noreply.github.com>
2021-09-23 02:10:27 +1200

    Update SetupAnalysisMachine.sh

--- SetupAnalysisMachine.sh
+++ SetupAnalysisMachine.sh
@@ -81,46 +81,17 @@ echo "Software installation complete. Testing operation of gr-satellites..."
 gr_satellites "Suomi 100" --wavfile ~/Documents/satellite-recordings/suomi_100.wav
 
 
+last4lines= "sw_count = 0
 
-expected_output="gr::log :DEBUG: correlate_access_code_tag_bb0 - Access code: 930b51de
-gr::log :DEBUG: correlate_access_code_tag_bb0 - Mask: ffffffff
-gr::log :DEBUG: correlate_access_code_tag_bb0 - writing tag at sample 3783
-gr::log :DEBUG: correlate_access_code_tag_bb0 - writing tag at sample 4863
-***** VERBOSE PDU DEBUG PRINT ******
-((transmitter . 9k6 FSK downlink))
-pdu length = 96 bytes
-pdu vector contents = 
-0000: 01 80 a7 82 00 b9 fd f5 e8 01 23 01 23 01 37 1f 
-0010: c9 00 1c 00 01 00 50 00 03 00 3e 00 6f 00 b2 00 
-0020: b2 00 23 00 09 00 36 00 99 00 00 00 02 00 03 00 
-0030: 04 00 00 00 00 03 b9 fd f5 e8 00 48 00 46 ff 9b 
-0040: f2 55 ff 9c b9 fd f5 e8 00 01 00 00 00 01 00 02 
-0050: 00 00 03 4d 00 4c 00 50 17 6b 1a f5 8b db ce af 
-************************************
-***** VERBOSE PDU DEBUG PRINT ******
-((transmitter . 9k6 FSK downlink))
-pdu length = 136 bytes
-pdu vector contents = 
-0000: 01 80 a7 82 01 b9 fd f5 e8 00 00 1b e2 00 01 16 
-0010: b6 00 00 00 06 00 00 00 00 00 00 00 00 00 00 00 
-0020: 00 00 00 00 00 00 00 07 00 00 00 00 00 00 00 00 
-0030: 00 00 00 00 01 01 01 01 01 01 00 00 01 b9 fd f5 
-0040: e8 00 00 02 58 a5 00 01 62 92 01 66 63 90 00 a3 
-0050: 09 c8 07 64 00 00 00 01 00 06 63 12 00 00 00 15 
-0060: 02 00 00 0a 9b 00 00 00 15 b9 fd f5 e8 01 01 01 
-0070: 01 00 00 00 00 01 0a 6e 00 00 00 01 b9 fd f5 e7 
-0080: c9 f4 1f b0 5a ed 03 ef 
-************************************" 
+            filesystem = 1
+
+            boot_count = 2670
+
+            boot_cause = 1
+
+            clock = 2068-11-18 01:41:59"
 
 
-satname='"Suomi 100"'
-
-ActualOutput="$(gr_satellites "$satname" --wavfile ~/Documents/satellite-recordings/suomi_100.wav --hexdump)"
-
-if [[ $ActualOutput == $expected_output ]] ; then
-		echo "Test passed - gr-satellites is installed and working!"
-	else
-		echo "Test failed - output hex dumps do not match."
-fi
+echo "If you see several lines of telemetry data, ending with $last4lines , gr-satellites was correctly installed"
 
 
